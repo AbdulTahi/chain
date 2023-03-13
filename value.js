df6808c3da178900aeede387ba188be8ea3a3cff
AbdulTahi <aattahir4@gmail.com>
2023-03-13 11:43:00 +0100

    Update value.js

--- value.js
+++ value.js
@@ -1,6 +1,6 @@
 let chain = document.querySelectorAll('#demo');
 for(let i=0; i < chain.length; i++) {
     chain[i].addEventListener('click', function() {
-        console.log(chain[i].innerText);
+        alert(chain[i].innerText);
     });
 }
