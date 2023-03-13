972f0c9f7b764f578b9d43dd734233dac7b398a5
AbdulTahi <aattahir4@gmail.com>
2023-03-13 11:40:16 +0100

    Update value.js

--- value.js
+++ value.js
@@ -1,6 +1,6 @@
-function passval()
-{
-    var selecttext=document.getElementById('demo');
-    localStorage.setItem('name', selecttext);
-    return true;
+let chain = document.querySelectorAll('#demo');
+for(let i=0; i < chain.length; i++) {
+    chain[i].addEventListener('click', function() {
+        console.log(chain[i].innerText);
+    });
 }
